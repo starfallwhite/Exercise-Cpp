67d8b9ef2ca0043514a95b0b0b11d24d95d45ae7
guzhu <131564306+starfallwhite@users.noreply.github.com>
2025-10-15 12:30:33 +0800

    编写至分组打分环节

--- SpeechCompetition/main.cpp
+++ SpeechCompetition/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<string>
-
+#include<map>
+#include<random>
 using namespace std;
 
 //建立演讲管理类封装程序
@@ -10,10 +12,18 @@ public:
     speechManage(){
 
     }
+    //第一轮12人,选手id
+    vector<int> First_speecher;
+    //第二轮6人
+    vector<int> Second_speecher;
+    //决胜者3人
+    vector<int> Last_speecher;
+    //id对应选手map
+    map<int,speecher> m_speecher;
+
 
     //显示菜单
     void showmenu();
-
     //开始比赛
     void startGame();
     //保存数据
@@ -22,6 +32,15 @@ public:
 
     //退出程序
     void exitSys();
+
+    //创建选手
+    void createSpeech();
+    //第一轮比赛分组打分
+    void firstecohGame();
+    //比赛打分,六个人
+    void speecheScore(vector<int>v_id);
+    //打印选手得分信息  id name score
+    void printSpeecher(vector<int>v_id);
 };
 
 //选手类
@@ -31,8 +50,15 @@ public:
         m_Name = Name;
         m_Score = Score;
     }
-    string showName(){return m_Name;}
-    float showScore(){return m_Score;}
+    
+    speecher(const speecher& other){
+        m_Name = other.m_Name;
+        m_Score = other.m_Score;
+    }
+    string getName(){return m_Name;}
+    float getScore(){return m_Score;}
+    void setScore(float score){m_Score = score;}//修改分数
+
 
 private:
     string m_Name;
@@ -52,25 +78,77 @@ void speechManage::showmenu(){
     cout<<"**********************************************************"<<endl;
 }
 
-//初始化参赛选手
+//初始化创建参赛选手
+void speechManage::createSpeech(){
+    string nameseed = "ABCDEFGHIJKL";
+    string name = "选手";
+    int id = 10001;
+    for(int i = 0; i < 12; i++){
+        name += nameseed[i];
+        id += i;
+        speecher sp(name,0);
 
+        this->First_speecher.push_back(id);
+        this->m_speecher.insert(make_pair(id,sp));
 
-//选手分组
+        name = "选手";
+    }
+}
 
+//选手分组打分
+void speechManage::firstecohGame(){
+    //随机分组
+    random_shuffle(First_speecher.begin(),First_speecher.end());
+    vector<int>v1(First_speecher.begin(),First_speecher.begin()+First_speecher.size()/2);
+    vector<int>v2(First_speecher.begin()+First_speecher.size()/2,First_speecher.end());
+    //打分
+    speecheScore(v1);
+    speecheScore(v2);
+    //显示分组打分结果
+    
+    //排序
 
+    //返回晋级结果 Second_speecher
+
+    //打印结果
+}
+//打分
+void speechManage::speecheScore(vector<int>v_id){
+    float score = 0;
+    // 创建随机数引擎和分布
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> dis(0.0f,nextafter(100.0f,numeric_limits<float>::max()));//创建范围在[0,100.0]的随机分布
+    
+    for(vector<int>::iterator it = v_id.begin(); it != v_id.end(); it++){
+        map<int,speecher>::iterator mp_it = m_speecher.find(*it);
+        if(mp_it == m_speecher.end()){
+            cout<<"打分过程中未查询到该选手的id"<<endl;
+        }
+        else{
+            score = dis(gen);
+            (*mp_it).second.setScore(score);
+        }
+    }
+}
 //第一轮比赛  分组--打分--排序
 
 //第二轮比赛    打分---排序
 
 //1.开始比赛
 void speechManage::startGame(){
-    //第一轮12人
-    vector<speecher> First_speecher;
+    //创建选手
+    createSpeech();
+    //第一轮12人，分组打分
+    firstecohGame();
+    
+    
+    //分组并打分
 
     //第二轮6人
     vector<speecher> Second_speecher;
 
-    
+
     //决胜者3人
     vector<speecher> Last_speecher;
 
@@ -93,6 +171,7 @@ void test01(){
 }
 
 int main(){
+    srand((unsigned int)time(NULL));
     speechManage speech;
     
     int chioce = 0;
